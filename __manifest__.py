39a173e60b7e795bb3b8e2f3b50012ccfd7b0338
a19gabrielbi <a19gabrielbi@iessanclemente.net>
2023-03-14 17:01:50 +0100

    cambiado nombre de fichero views a logros

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/logros.xml',
         'views/historial.xml',
     ],
     # only loaded in demonstration mode
