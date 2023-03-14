723dbd189834f8064cef87b71330db7851669057
a19gabrielbi <a19gabrielbi@iessanclemente.net>
2023-03-14 16:58:45 +0100

    eliminadas clases inutilizables

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
         'views/historial.xml',
     ],
     # only loaded in demonstration mode
