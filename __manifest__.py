84716f0b83ef2c5f98087da109ac5942d69d3ccf
a19gabrielbi <a19gabrielbi@iessanclemente.net>
2023-03-14 21:37:14 +0100

    extras añadidos

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "LogrosOdoo",
+    'name': "Logros odoo",
 
     'summary': """
         Pequeña aplicacion que implementa logros en odoo""",
@@ -10,12 +10,12 @@
     """,
 
     'author': "A19GabrielBI",
-    'website': "https://github.com/A19GabrielBI/LogrosOdoo",
+    'website': "https://github.com/A19GabrielBI/logros_odoo",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Productivity',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
--- models/models.py
+++ models/models.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import re
 
 class logros_odoo(models.Model):
     _name = 'logros_odoo.logros_odoo'
     _description = 'logros_odoo.logros_odoo'
 
     nombre = fields.Char(string="Nombre", required=True)
-    imagen = fields.Binary(string="Imagen")
+    imagen = fields.Binary(string="Icono")
     descripcion = fields.Text(string="Descripcion", required=True)
     categoria =  fields.Char(string="Categoria", required=True)
+
+    _sql_constraints = [
+        ('nombre_categoria_unicos', 'unique(nombre, categoria)', 'Este logro ya está definido con la misma categoria.')
+    ]
 
     def name_get(self):
         result = []
@@ -23,10 +27,19 @@ class empleado_logro(models.Model):
     _name='empleado.logro'
     _description='empleado.logro'
 
-    nombre = fields.Many2one('logros_odoo.logros_odoo', string="Logro")
+    nombre = fields.Many2one('logros_odoo.logros_odoo', string="Logro", required=True)
     fecha_evento = fields.Datetime(string="Fecha logro", default=fields.datetime.now(), required=True)
-    name = fields.Many2one('hr.employee', string="Empleado")
-    resultado = fields.Selection([("bronce", "Bronce"),("plata", "Plata"),("oro", "Oro")], required=True, string="Nivel")
+    name = fields.Many2one('hr.employee', string="Empleado", required=True)
+    informacion_extra = fields.Char(string="Información")
+
+    imagen_logro = fields.Binary(string="Icono", compute='_compute_imagen_logro')
+    @api.depends('nombre')
+    def _compute_imagen_logro(self):
+        for record in self:
+            if record.nombre and record.nombre.imagen:
+                record.imagen_logro = record.nombre.imagen
+            else:
+                record.imagen_logro = False
 
     def name_get(self):
         result = []
